7d51865c9c472b09ea83153d2ebce65da776dd2d
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-05-27 11:07:18 -0700

    add step

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
-const todos = require("./routes/api/todos")
+const todos = require("./routes/api/todos");
+const steps = require("./routes/api/steps");
+
 const bodyParser = require("body-parser");
 const passport = require('passport');
 
@@ -13,6 +15,8 @@ app.use( bodyParser.urlencoded({ extended: false }));
 app.use( bodyParser.json());
 app.use("/api/users",users);
 app.use("/api/todos",todos);
+app.use("/api/steps",steps);
+
 mongoose
     .connect( db, { useNewUrlParser: true })
     .then(() => console.log("connected to mongodb"))
