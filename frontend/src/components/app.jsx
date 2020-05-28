3738aa1d9c782de278ec98a21c98e7dc1e03e99d
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-05-27 20:11:00 -0700

    add reducers, utils,session actions

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { AuthRoute , ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
 import TodoListContainer  from './todos/todo_list_container'
 
-const App = ()=>{    
-    return(   
-        <div>
-           <TodoListContainer />      
-        </div>
-    );
-}   
+import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+
+const App = ()=>(  
+    <div>
+        <TodoListContainer />      
+    <Switch>    
+        <AuthRoute exact path="/" component={ MainPage}/>      
+    </Switch>
+    </div>
+);
+   
+
+
+// const App = ()=>{    
+//     return(   
+//         <div>
+//            <TodoListContainer />      
+//         </div>
+//     );
+// }   
      
 export default App;
--- frontend/src/todo_redux.jsx
+++ frontend/src/todo_redux.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import  configureStore  from './store/store';
 // import { receiveTodos , receiveTodo } from './actions/todo_actions'
 import Root  from './components/root';
+
+// We will use this to parse the user's session token
+import jwt_decode from 'jwt-decode';
+
+import { setAuthToken } from './util/session_api_util';
+
+import { logout } from './actions/session_actions';
+
+
+
 // import { Provider } from 'react-redux';
 // import { stepsByTodoId } from './reducers/selectors' ;
 // import { receiveSteps , receiveStep , removeStep } from './actions/step_actions';
@@ -22,9 +32,8 @@ import Root  from './components/root';
 // window.StepList = StepList;
 
 
-
-
-document.addEventListener("DOMContentLoaded",()=>{   
+document.addEventListener("DOMContentLoaded",()=>{ 
+      
     const preloadedState = localStorage.state ? 
         JSON.parse(localStorage.state) : {};        
     const store = configureStore(preloadedState);
@@ -32,6 +41,49 @@ document.addEventListener("DOMContentLoaded",()=>{
     ReactDOM.render(<Root store={store} />,document.getElementById('root') );
     
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+
+     // If a returning user has a session token stored in localStorage
+    if( localStorage.jwtToken){
+
+// Set the token as a common header for all axios requests
+        setAuthToken( localStorage.jwtToken );
+
+  // Set the token as a common header for all axios requests
+        const decodedUser = jwt_decode( localStorage.jwtToken );
+
+        const preloadedState = { session: { isAuthenticated : true, user : decodedUser} }
+
+        const store = configureStore(preloadedState);
+
+        const curentTime = Date.now()/1000;
+
+        if( decodedUser.exp < curentTime ) {
+             // Logout the user and redirect to the login page
+             store.dispatch( logout());
+             window.location.href = '/login'
+        }
+    }else {
+            // If this is a first time user, start with an empty store
+            store = configureStore({});
+    }
+
+    // Render our root component and pass in the store as a prop
+    ReactDOM.render(<Root store={store} />,document.getElementById('root') );
+});
+
+//This was the code before I added the backend part
+/*document.addEventListener("DOMContentLoaded",()=>{ 
+      
+    const preloadedState = localStorage.state ? 
+        JSON.parse(localStorage.state) : {};        
+    const store = configureStore(preloadedState);
+
+    ReactDOM.render(<Root store={store} />,document.getElementById('root') );
+    
+});*/
 
 
 
