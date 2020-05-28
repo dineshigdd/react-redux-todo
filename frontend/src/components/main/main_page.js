ef83b4053648a1412d12b346bb6ba1b2bf39938f
dineshigdd@gmail.com <dineshigdd@gmail.com>
2020-05-27 20:11:58 -0700

    add new files to frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>A Todo App</h1>
+                <footer>
+                    Copyright &copy; 2020 Dinesh
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
